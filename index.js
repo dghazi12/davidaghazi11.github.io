b09ff5f85c179f7669315dd63c801288692c2a92
David Ghazi <davidaghazi@gmail.com>
2020-04-20 19:33:26 -0400

    server is working properly

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// Dependencies
+// =============================================================
 const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });  
